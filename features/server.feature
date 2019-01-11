cb68ef2efa8a5a4862dbf22761d35e0b7497ee64
huangyan <huangyan@actionsky.com>
2019-01-11 16:03:52 +0800

    add test on sippool/remove

--- features/server.feature
+++ features/server.feature
@@ -36,26 +36,47 @@ Feature: server
 		| ustats |
 
 	@test @case.272
-	Scenario: sippool/add should succeed
+	Scenario: sippool/add and sippool/remove should succeed
 	  When I found a ip segments xx.xx.0.0/16 which not contains in sip pool
 	  And I add ip xx.xx.1.2 to sip pool
 	  Then the response is ok
-	  And the sip pool should contains xx.xx.1.2
+	  And the sip pool should contain xx.xx.1.2
 
 	  When I add ip xx.xx.1.3-xx.xx.1.5 to sip pool
 	  Then the response is ok
-	  And the sip pool should contains xx.xx.1.3,xx.xx.1.4,xx.xx.1.5
+	  And the sip pool should contain xx.xx.1.3,xx.xx.1.4,xx.xx.1.5
 
 	  When I add ip xx.xx.1.6,xx.xx.1.8 to sip pool
 	  Then the response is ok
-	  And the sip pool should contains xx.xx.1.6,xx.xx.1.8
+	  And the sip pool should contain xx.xx.1.6,xx.xx.1.8
 
 	  When I add ip xx.xx.1.9,xx.xx.1.10-xx.xx.1.20 to sip pool
 	  Then the response is ok
-	  And the sip pool should contains xx.xx.1.6,xx.xx.1.8
+	  And the sip pool should contain xx.xx.1.10,xx.xx.1.20
 	  And the sip pool should have 18 ips match xx.xx.1.*
 
 	  When I add ip xx.xx.2.0/24 to sip pool
 	  Then the response is ok
-	  And the sip pool should contains xx.xx.2.1,xx.xx.2.123,xx.xx.2.123
+	  And the sip pool should contain xx.xx.2.1,xx.xx.2.123,xx.xx.2.123
 	  And the sip pool should have 254 ips match xx.xx.2.*
+
+	  When I remove ip xx.xx.2.0/24 from sip pool
+	  Then the response is ok
+	  And the sip pool should not contain xx.xx.2.1,xx.xx.2.123,xx.xx.2.123
+	  And the sip pool should have 0 ips match xx.xx.2.*
+
+	  When I remove ip xx.xx.1.9,xx.xx.1.10-xx.xx.1.20 from sip pool
+	  Then the response is ok
+	  And the sip pool should not contain xx.xx.1.10,xx.xx.1.20
+
+	  When I remove ip xx.xx.1.6,xx.xx.1.8 from sip pool
+	  Then the response is ok
+	  And the sip pool should not contain xx.xx.1.6,xx.xx.1.8
+
+	  When I remove ip xx.xx.1.3-xx.xx.1.5 from sip pool
+	  Then the response is ok
+	  And the sip pool should not contain xx.xx.1.3,xx.xx.1.4,xx.xx.1.5
+	  
+	  When I remove ip xx.xx.1.2 from sip pool
+	  Then the response is ok
+	  And the sip pool should not contain xx.xx.1.2
--- features/steps/server.py
+++ features/steps/server.py
@@ -93,8 +93,7 @@ def step_impl(context, comp, expect_own_user, expect_own_group):
 
 @when(u'I found a ip segments xx.xx.0.0/16 which not contains in sip pool')
 def step_impl(context):
-    resp = api_get(context, "sippool/list")
-    exist_ips = pyjq.all('.[] | .sip', resp)
+    exist_ips = get_sippool_all_ips(context)
 
     context.free_ip_segments = None
 
@@ -116,15 +115,22 @@ def step_impl(context, ip_desc):
         "sip": ip_desc,
     })
 
+@when(u'I remove ip {ip_desc:string} from sip pool')
+def step_impl(context, ip_desc):
+    assert context.free_ip_segments != None
+
+    ip_desc = ip_desc.replace("xx.xx", context.free_ip_segments)
+    api_request_post(context, "sippool/remove", {
+        "is_sync": "true",
+        "sip": ip_desc,
+    })
+
 @then(u'the sip pool should have {expect_count:int} ips match {ip_pattern:string}')
 def step_impl(context, expect_count, ip_pattern):
     assert context.free_ip_segments != None
+    exist_ips = get_sippool_all_ips(context)
 
     ip_prefix = ip_pattern.rstrip("*").replace("xx.xx", context.free_ip_segments)
-
-    resp = api_get(context, "sippool/list")
-    exist_ips = pyjq.all('.[] | .sip', resp)
-
     count = 0
     for ip in exist_ips:
         if ip.startswith(ip_prefix):
@@ -133,13 +139,24 @@ def step_impl(context, expect_count, ip_pattern):
     assert count == expect_count
 
 
-@then(u'the sip pool should contains {expect_ips:strings+}')
+@then(u'the sip pool should contain {expect_ips:strings+}')
 def step_impl(context, expect_ips):
     assert context.free_ip_segments != None
-
-    resp = api_get(context, "sippool/list")
-    exist_ips = pyjq.all('.[] | .sip', resp)
+    exist_ips = get_sippool_all_ips(context)
 
     for expect_ip in expect_ips:
         expect_ip = expect_ip.replace("xx.xx", context.free_ip_segments)
         assert expect_ip in exist_ips
+
+@then(u'the sip pool should not contain {expect_ips:strings+}')
+def step_impl(context, expect_ips):
+    assert context.free_ip_segments != None
+    exist_ips = get_sippool_all_ips(context)
+
+    for expect_ip in expect_ips:
+        expect_ip = expect_ip.replace("xx.xx", context.free_ip_segments)
+        assert not (expect_ip in exist_ips)
+
+def get_sippool_all_ips(context):
+    resp = api_get(context, "sippool/list")
+    return pyjq.all('.[] | .sip', resp)
