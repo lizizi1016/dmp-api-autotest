b2232147e746e5e305788d73f25918151cd96e91
huangyan <huangyan@actionsky.com>
2019-02-21 20:40:49 +0800

    add test on "remove ushard should succeed"

--- features/steps/ushard.py
+++ features/steps/ushard.py
@@ -39,3 +39,25 @@ def step_impl(context, should_or_not):
 	})
 	match = pyjq.first('.data[] | select(.group_id == "{0}")'.format(group_id), resp)
 	assert (match != None and should_or_not) or (match == None and not should_or_not)
+
+
+@when(u'I found a Ushard group without Ushard instance, or I skip the test')
+def step_impl(context):
+	resp = api_get(context, "ushard/list_group", {
+		"number": context.page_size_to_select_all,
+	})
+	match = pyjq.first('.data[] | select(.group_instance_num == "0")', resp)
+	if match is None:
+		context.scenario.skip("Found no Ushard group without Ushard instance")
+	else:
+		context.ushard_group = match
+
+@when(u'I remove the Ushard group')
+def step_impl(context):
+	assert context.ushard_group != None
+	group_id = context.ushard_group["group_id"]
+
+	api_request_post(context, "ushard/remove_group", {
+		"is_sync": True,
+		"group_id": group_id,
+	})
--- features/ushard.feature
+++ features/ushard.feature
@@ -6,3 +6,10 @@ Feature: ushard
 	  And I add a Ushard group
 	  Then the response is ok
 	  And the Ushard group list should contains the Ushard group
+
+	@test
+	Scenario: remove ushard should succeed
+	  When I found a Ushard group without Ushard instance, or I skip the test
+	  And I remove the Ushard group
+	  Then the response is ok
+	  And the Ushard group list should not contains the Ushard group
