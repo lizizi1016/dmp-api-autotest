bdc8f10272b922460add94977c2bb5cf67abae34
zhangjingwen <zhangjingwen@actionsky.com>
2019-03-11 18:21:49 +0800

    add remove SLA HA policy template

--- features/highly_available_policy.feature
+++ features/highly_available_policy.feature
@@ -1,12 +1,18 @@
 Feature: Highly Available policy
   
   Scenario: MySQL004-Highly Available policy add RTO template
-	When I add a RTO template
+	When I add a rto template
 	Then the response is ok
-	And the Highly Available policy list should contains the RTO template
+	And the Highly Available policy list should contains the rto template
   
   Scenario: MySQL007-update RTO template configuration
 	When I found a valid rto template, or I skip the test
 	And I modify the rto template configuration, sla_rto: 700, sla_rto_levels: 20,30,400
 	Then the response is ok
 	And update the rto template configuration successful in 5s
+  
+  Scenario: MySQL008-remove a Highly Available policy template
+	When I found a valid rto template, or I skip the test
+	And I remove the rto template
+	Then the response is ok
+	And the rto template should be removed successfully in 5s
--- features/steps/highly_available_policy.py
+++ features/steps/highly_available_policy.py
@@ -4,22 +4,22 @@ from common import *
 use_step_matcher("cfparse")
 
 
-@when(u'I add a RTO template')
-def step_impl(context):
-    rto_template_name = "api_rto_" + generate_id()
+@when(u'I add a {type:string} template')
+def step_impl(context, type):
+    rto_template_name = "api_{0}_".format(type) + generate_id()
     template_params = {
         "name": rto_template_name,
         "sla_rto": 300,
         "sla_rto_levels": "10,50,200",
         "sla_rto_te": "500",
-        "sla_type": "rto"
+        "sla_type": type
     }
     api_request_post(context, "sla/add", template_params)
     context.rto_template_name = {"name": rto_template_name}
 
 
 @then(
-    u'the Highly Available policy list {should_or_not:should_or_not} contains the RTO template'
+    u'the Highly Available policy list {should_or_not:should_or_not} contains the rto template'
 )
 def step_impl(context, should_or_not):
     assert context.rto_template_name != None
@@ -87,3 +87,30 @@ def step_imp(context, type, duration):
             return True
 
     waitfor(context, condition, duration)
+
+
+@when(u'I remove the {type:string} template')
+def step_imp(context, type):
+    assert context.rto_template_name != None
+    template_params = {
+        "name": context.rto_template_name,
+        "sla_rto_te": "500",
+        "sla_type": type
+    }
+    api_request_post(context, "sla/remove", template_params)
+
+
+@then(u'the {type:string} template {should_or_not:should_or_not} be removed successfully in {duration:time}')
+def step_imp(context, type, should_or_not, duration):
+    assert context.rto_template_name != None
+
+    def remove_template(context, flag):
+        res = api_get(context, "sla/list")
+        match = pyjq.first(
+            '.[] | select(."name"=="{0}") | select(."sla_type"=="{1}")'.format(context.rto_template_name, type), res)
+
+        print(match)
+        if (match == None and should_or_not) or (match != None and not should_or_not):
+            return True
+
+    waitfor(context, remove_template, duration)
