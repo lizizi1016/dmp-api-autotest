25391388dada092bde15001ec0a88d13e05bc5d7
zhangjingwen <zhangjingwen@actionsky.com>
2019-03-12 13:08:43 +0800

    update sla template test cases

--- features/highly_available_policy.feature
+++ features/highly_available_policy.feature
@@ -1,18 +1,36 @@
 Feature: Highly Available policy
   
-  Scenario: MySQL004-Highly Available policy add RTO template
-	When I add a rto template
+  @wip
+  Scenario Outline: MySQL004-Highly Available policy add RTO/RPO template
+	When I add a <type> template
 	Then the response is ok
-	And the Highly Available policy list should contains the rto template
+	And the Highly Available policy list should contains the <type> template
+	
+	Examples: sla type
+	  | type |
+	  | rto  |
+	  | rpo  |
   
-  Scenario: MySQL007-update RTO template configuration
-	When I found a valid rto template, or I skip the test
-	And I modify the rto template configuration, sla_rto: 700, sla_rto_levels: 20,30,400
+  @wip
+  Scenario Outline: MySQL007-update RTO/RPO template configuration
+	When I found a valid <type> template, or I skip the test
+	And I update the <type> template configuration, <config>
 	Then the response is ok
-	And update the rto template configuration successful in 5s
+	And the <type> template should exist
+	
+	Examples: update config
+	  | type | config                                                                 |
+	  | rto  | sla_rto: 700, sla_rto_levels: 20,30,400                                |
+	  | rpo  | sla_rpo:1, sla_rpo_levels:  10,40,500, sla_rpo_error_levels: 20,50,500 |
   
-  Scenario: MySQL008-remove a Highly Available policy template
-	When I found a valid rto template, or I skip the test
-	And I remove the rto template
+  
+  Scenario Outline: MySQL008-remove a Highly Available policy template
+	When I found a valid <type> template, or I skip the test
+	And I remove the <type> template
 	Then the response is ok
-	And the rto template should be removed successfully in 5s
+	And the <type> template should not exist
+	
+	Examples: sla type
+	  | type |
+	  | rto  |
+	  | rpo  |
--- features/steps/highly_available_policy.py
+++ features/steps/highly_available_policy.py
@@ -6,27 +6,39 @@ use_step_matcher("cfparse")
 
 @when(u'I add a {type:string} template')
 def step_impl(context, type):
-    rto_template_name = "api_{0}_".format(type) + generate_id()
-    template_params = {
-        "name": rto_template_name,
-        "sla_rto": 300,
-        "sla_rto_levels": "10,50,200",
-        "sla_rto_te": "500",
-        "sla_type": type
-    }
+    sla_template = "api_{0}_".format(type) + generate_id()
+    template_params = {}
+    if type == 'rto':
+        template_params = {
+            "name": sla_template,
+            "sla_rto": 300,
+            "sla_rto_levels": "10,50,200",
+            "sla_rto_te": "500",
+            "sla_type": type
+        }
+    elif type == 'rpo':
+        template_params = {
+            "name": sla_template,
+            "sla_rpo": 0,
+            "sla_rpo_levels": "10,50,200",
+            "sla_rpo_error_levels": "20,60,600",
+            "sla_type": type
+        }
+
+    print(template_params)
     api_request_post(context, "sla/add", template_params)
-    context.rto_template_name = {"name": rto_template_name}
+    context.sla_template = {"name": sla_template}
 
 
 @then(
-    u'the Highly Available policy list {should_or_not:should_or_not} contains the rto template'
+    u'the Highly Available policy list {should_or_not:should_or_not} contains the {type:string} template'
 )
-def step_impl(context, should_or_not):
-    assert context.rto_template_name != None
-    rto_template_name = context.rto_template_name["name"]
+def step_impl(context, should_or_not, type):
+    assert context.sla_template != None
+    sla_template = context.sla_template["name"]
 
     resp = api_get(context, "sla/list")
-    match = pyjq.first('.[] | select(."name"=="{0}")'.format(rto_template_name),
+    match = pyjq.first('.[] | select(."name"=="{0}") | select(."sla_type"=="{1}")'.format(sla_template, type),
                        resp)
     assert (match != None and should_or_not) or (match == None and
                                                  not should_or_not)
@@ -39,75 +51,92 @@ def step_impl(context, type):
     resp = api_get(context, "sla/list")
     match = pyjq.first(condition, resp)
     if match is None:
-        context.scenario.skip("No valid rto template found")
+        context.scenario.skip("No valid {0} template found".format(type))
         return
-    context.rto_template_name = match["name"]
-    context.sla_rto_levels = match["sla_rto_levels"]
-    context.sla_rto = match["sla_rto"]
+    if type == 'rto':
+        context.sla_rto_levels = match['sla_rto_levels']
+        context.sla_rto = match['sla_rto']
+    elif type == 'rpo':
+        context.sla_rpo = match['sla_rpo']
+        context.sla_rpo_levels = match['sla_rpo_levels']
+        context.sla_rpo_error_levels = match['sla_rpo_error_levels']
+    context.sla_template = match["name"]
 
 
 @when(
-    u'I modify the {type:string} template configuration, {template_values:option_values}'
+    u'I update the {type:string} template configuration, {template_values:option_values}'
 )
 def step_impl(context, type, template_values):
-    assert context.rto_template_name != None
-    assert context.sla_rto_levels != None
-    assert context.sla_rto != None
+    assert context.sla_template != None
+    template_params = {}
+    if type == 'rto':
+        template_params = {
+            "name": context.sla_template,
+            "sla_rto": template_values['sla_rto'],
+            "sla_rto_levels": template_values['sla_rto_levels'],
+            "sla_rto_te": "500",
+            "sla_type": type
+        }
+        context.sla_rto_levels = template_values['sla_rto_levels']
+        context.sla_rto = template_values['sla_rto']
+    elif type == 'rpo':
+        template_params = {
+            "name": context.sla_template,
+            "sla_rpo": template_values['sla_rpo'],
+            "sla_rpo_levels": template_values['sla_rpo_levels'],
+            "sla_rpo_error_levels": template_values['sla_rpo_error_levels'],
+            "sla_type": type
+        }
+        context.sla_rpo = template_values['sla_rpo']
+        context.sla_rpo_levels = template_values['sla_rpo_levels']
+        context.sla_rpo_error_levels = template_values['sla_rpo_error_levels']
 
-    template_params = {
-        "name": context.rto_template_name,
-        "sla_rto": template_values['sla_rto'],
-        "sla_rto_levels": template_values['sla_rto_levels'],
-        "sla_rto_te": "500",
-        "sla_type": type
-    }
     api_request_post(context, "sla/update", template_params)
-    context.sla_rto_levels = template_values['sla_rto_levels']
-    context.sla_rto = template_values['sla_rto']
 
 
-@then(
-    u'update the {type:string} template configuration successful in {duration:time}'
-)
-def step_imp(context, type, duration):
-    assert context.rto_template_name != None
-    assert context.sla_rto_levels != None
-    assert context.sla_rto != None
+@then(u'the {type:string} template {should_or_not:should_or_not} exist')
+def step_imp(context, type, should_or_not):
+    assert context.sla_template != None
+    match = None
+    if type == 'rto':
+        assert context.sla_rto_levels != None
+        assert context.sla_rto != None
 
-    def condition(context, flag):
         resp = api_get(context, "sla/list")
         condition = '.[] | select(."name"=="{0}") | select(."sla_rto_levels"=="{1}") | select(."sla_rto"=="{2}") | select(."sla_type"=="{3}")'.format(
-            context.rto_template_name, context.sla_rto_levels, context.sla_rto,
-            type)
+            context.sla_template, context.sla_rto_levels, context.sla_rto, type)
+        match = pyjq.first(condition, resp)
+    elif type == 'rpo':
+        assert context.sla_rpo != None
+        assert context.sla_rpo_levels != None
+        assert context.sla_rpo_error_levels != None
+
+        resp = api_get(context, "sla/list")
+        condition = '.[] | select(."name"=="{0}") | select(."sla_rpo_levels"=="{1}") | select(."sla_rpo_error_levels"=="{2}") | select(."sla_rpo"=="{3}") | select(."sla_type"=="{4}")'.format(
+            context.sla_template, context.sla_rpo_levels, context.sla_rpo_error_levels, context.sla_rpo, type)
         match = pyjq.first(condition, resp)
 
-        if match == None:
-            return
-        if match != None:
-            return True
-
-    waitfor(context, condition, duration)
+    assert (match != None and should_or_not) or (match == None and not should_or_not)
 
 
 @when(u'I remove the {type:string} template')
 def step_imp(context, type):
-    assert context.rto_template_name != None
+    assert context.sla_template != None
     template_params = {
-        "name": context.rto_template_name,
-        "sla_rto_te": "500",
+        "name": context.sla_template,
         "sla_type": type
     }
     api_request_post(context, "sla/remove", template_params)
 
 
-@then(u'the {type:string} template {should_or_not:should_or_not} be removed successfully in {duration:time}')
+@then(u'the {type:string} template {should_or_not:should_or_not} exist')
 def step_imp(context, type, should_or_not, duration):
-    assert context.rto_template_name != None
+    assert context.rto_template != None
 
     def remove_template(context, flag):
         res = api_get(context, "sla/list")
         match = pyjq.first(
-            '.[] | select(."name"=="{0}") | select(."sla_type"=="{1}")'.format(context.rto_template_name, type), res)
+            '.[] | select(."name"=="{0}") | select(."sla_type"=="{1}")'.format(context.rto_template, type), res)
 
         print(match)
         if (match == None and should_or_not) or (match != None and not should_or_not):
